9356a2de35f4a426f5e5a6083d554c51ac54113d
NFarhan11 <najmifarhan9903@gmail.com>
2025-07-24 05:40:42 +0800

    toggle cells between live or dead

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,7 +9,8 @@
 
     <div class="grid-container">
       <div v-for="(row, rowIndex) in grid" :key="rowIndex">
-        <div v-for="(cell, colIndex) in row" :key="colIndex" class="cell">
+        <div v-for="(cell, colIndex) in row" :key="colIndex" :class="['cell', { 'cell-alive': cell === 1 }]"
+          @click="toggleCell(rowIndex, colIndex)">
         </div>
       </div>
     </div>
@@ -18,7 +19,7 @@
 
 <script setup>
 const gridSize = 30;  // 30x30 grid
-const cellSize = 15;  // 15px in CSS
+const cellSize = '15px';  // 15px in CSS
 const grid = ref([]);
 const isRunning = ref(false);
 
@@ -28,6 +29,16 @@ const initializeGrid = () => {
   const newGrid = Array(gridSize).fill(0).map(() => Array(gridSize).fill(0));
   grid.value = newGrid;
 };
+
+// Toggling Cell
+const toggleCell = (row, col) => {
+  // create deep copy of current grid.value
+  let tempGrid = structuredClone(toRaw(grid.value));
+  // access cell at row,col in copied grid, toggle its value 0-1
+  tempGrid[row][col] = tempGrid[row][col] === 1 ? 0 : 1;
+  // assign copied grid to original
+  grid.value = tempGrid
+}
 
 onMounted(() => {
   initializeGrid();
@@ -43,11 +54,16 @@ onMounted(() => {
 }
 
 .cell {
-  width: v-bind(cellSize + 'px');
-  height: v-bind(cellSize + 'px');
+  /* cell-dead*/
+  width: v-bind(cellSize);
+  height: v-bind(cellSize);
   border: 2px solid;
   background-color: #f0f0f0;
   cursor: pointer;
+}
+
+.cell-alive {
+  background-color: greenyellow;
 }
 
 .controls {
